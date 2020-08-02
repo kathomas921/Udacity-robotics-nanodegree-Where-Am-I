82d52396fb99647aaac6f7221327fad4d7e1eee5
Katie Gardner <katie.thomas.gardner@gmail.com>
2020-08-02 18:45:20 -0400

    Correctly looks at rgb value rather than each color individually for 255

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -8,11 +8,6 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // Request a service and pass the velocities to it to drive the robot
-    if (lin_x != 0.0 || ang_z != 0.0) {
-      ROS_INFO_STREAM("Driving toward white ball");
-    }
-
     // Request new velocities
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
@@ -27,21 +22,26 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    int white_pixel = 255;
     float lin_x = 0.0;
     float ang_z = 0.0;
-    
+
+    int red_pixel, green_pixel, blue_pixel, column_position = 0;
+
+    int left_barrier = static_cast<int>(img.step / 3);
+    int right_barrier = img.step - left_barrier;
+
     // Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    for (int i = 0; i < img.height * img.step; i++) {
-      if (img.data[i] == white_pixel) {
+    for (int i = 0; i < img.height * img.step; i += 3) {
+      red_pixel = img.data[i];
+      green_pixel = img.data[i+1];
+      blue_pixel = img.data[i+2];
 
-        int left_barrier = static_cast<int>(img.step / 3);
-        int right_barrier = img.step - left_barrier;
-        int column_position = i % img.step;
+      if (red_pixel == 255 && blue_pixel == 255 && green_pixel == 255) {
         lin_x = 0.1;
+        column_position = i % img.step;
 
         if (column_position <= left_barrier) {
           ang_z = 0.1;
